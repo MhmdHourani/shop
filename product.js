ea58317b8e3a06943744232c7ac3c22f06cc9b7f
Mohammed Hourani <m.hourani98@gmail.com>
2020-10-21 16:31:55 +0300

    fix error

--- product.js
+++
@@ -1,5 +0,0 @@
-// Initializing a client to return content in the store's primary language
-const client = Client.buildClient({
-	apiKey: 'b122d77d001bdaf6760b23ab9a6c7b5a',
-	domain: 'shoprozn.myshopify.com',
-});
